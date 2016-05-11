07ad317cea62aebb26b567b7d934eaa4cf684003
Rob van Bentem <vanbentem@unifact.eu>
2016-05-11 17:16:26 +0200

    better smoothing method

--- poc.py
+++ poc.py
@@ -2,43 +2,24 @@
 
 import math, time
 
-NUM_LEDS = 15
+NUM_LEDS = 16
 
 steps = {}
-size = 2
-pos = 2
+size = 4
+pos = -4
 maxint = 100
 direction = 1
-speed = 5
-count = 5
+speed = 1
+count = 0
 
 leds = {}
 
 while True:
 
-    steps = {}
     leds = {}
 
     waveleds = 2 * size + 1
 
-    prev = 0.0
-    for n in range(0, waveleds+1):
-
-        if n > size:
-            cur = math.sin(math.pi * (180 / (waveleds * speed) * (n+1 - (count % speed))) / 180)
-        else:
-            cur = math.sin(math.pi * (180 / (waveleds * speed) * (n + (speed - (count % speed)))) / 180)
-
-        steps[n] = cur
-        print("%.2f " % cur, end="")
-    print()
-
-
-    #print("lpos: %d lrange:%d ldir:%d lspd:%d" % (lpos, lrange, ldir, lspd))
-
-    offset = pos - size
-
-    prev = 0.0
     for n in range(0, NUM_LEDS):
         
         if n > pos:
@@ -46,61 +27,29 @@ while True:
         else:
             wavepos = n - (pos - size)
 
-
         if wavepos < 0 or n > pos+size or wavepos >= waveleds:
             leds[n] = '_.__ '
-            #print("-pos:%d wp:%d n:%d v:0.00" % (pos, wavepos, n))
             continue
 
-        # 0 1 2 3 4 5 6 7 8 9
-        #     S       S   
-        #         P   N         =    
-        #     N   P     
+        leds[n] = "%.2f " % math.sin(math.pi * (180 / (waveleds * speed) * ((wavepos * speed) + (speed - count))) / 180)
 
 
-    
-        if wavepos > 0:
-            nx = steps[wavepos-1]
+    for p in leds.keys():
+        if direction == 1:
+            print(leds[p], end="")
         else:
-            nx = 0.0
-        
-        diff = abs(steps[wavepos] - nx)
+            print(leds[NUM_LEDS-p-1], end="")
 
-        if count % speed == 0:
-            btw = steps[wavepos]
-        else:
-            btw = nx + (diff / speed * (count % speed))
-
-        tmp = steps[wavepos] - btw
-
-
-        leds[n] = "%.2f " % steps[wavepos]
-        #print("+pos:%d wp:%d n:%d v:%.2f" % (pos, wavepos, n, steps[wavepos]))
-
-
-
-    #for p in leds.keys():
-        #if direction == 1:
-        #    print(leds[p], end="")
-        #else:
-        #    print(leds[NUM_LEDS-p-1], end="")
-
-    #print("\n----")
     print()
+    
     time.sleep(0.1)
 
     count += 1
 
-    if count % speed == 0:
+    if count == speed:
         pos = pos + 1
+        count = 0
 
     if pos - size >= NUM_LEDS:
         direction = -direction
         pos = -size
-
-
-    continue
-
-    print(','.join(leds.values()))
-    #break
-    time.sleep(1.01)
